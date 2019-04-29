e0f533363c27ae3e230cd85dff5d0ffcf9e08253
Konstantin <konstantin90s@gmail.com>
2019-04-29 15:33:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace displaypnpevent
+{
+    static class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var execPathSingle = string.Empty;
+            var execPathMultiple = string.Empty;
+            var argsLength = args.Length;
+            switch (argsLength)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+            var screenLengthPrevious = Screen.AllScreens.Length;
+            while (true)
+            {
+                var screenCountCurrent = Screen.AllScreens.Length;
+                if (screenCountCurrent != screenLengthPrevious)
+                {
+                    Thread.Sleep(5000);
+                    if (screenCountCurrent <= 1)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                screenLengthPrevious = screenCountCurrent;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
