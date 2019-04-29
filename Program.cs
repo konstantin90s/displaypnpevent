bd480254c13a65924ed63cacfb3fe1f9f1e97fa3
Konstantin <konstantin90s@gmail.com>
2019-04-29 15:38:24 +0300

    Initial commit mishmash

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,39 +12,105 @@ namespace displaypnpevent
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOWMINIMIZED = 2;
+        const int SW_SHOW = 5;
+
         [STAThread]
         static int Main(string[] args)
         {
-            var execPathSingle = string.Empty;
-            var execPathMultiple = string.Empty;
+            var screenCountPrevious = SystemInformation.MonitorCount;
             var argsLength = args.Length;
-            switch (argsLength)
+            if (argsLength == 0)
             {
-                case 0:
-                    return 0;
-                case 1:
-                    break;
-                default:
-                    break;
+                Console.WriteLine(screenCountPrevious);
+                return 0;
             }
-            var screenLengthPrevious = Screen.AllScreens.Length;
+            var execPathMultiple = string.Empty;
+            var execPathMultipleArgs = string.Empty;
+            var execPathSingle = string.Empty;
+            var execPathSingleArgs = string.Empty;
+            if (argsLength >= 1)
+            {
+                execPathMultiple = args[0];
+                if (execPathMultiple.CompareTo("/h") == 0 ||
+                    execPathMultiple.CompareTo("-h") == 0 ||
+                    execPathMultiple.CompareTo("--help") == 0)
+                {
+                    Console.WriteLine("usage: " + System.AppDomain.CurrentDomain.FriendlyName +
+                                      " [path to executable] [path to executable]");
+                    Console.WriteLine(@"
+       /h or -h or --help           display this usage reference
+       [no arguments specified]     output the number of displays currently connected
+       [1st argument specified]     this path will be executed on connection of each additional display: 2nd, 3rd, etc
+       [2nd argument specified]     this path will be executed on display disconnection and only one left as a result");
+                    return 0;
+                }
+                var execPathMultipleArr = execPathMultiple.Split(new[] { "\"" }, StringSplitOptions.RemoveEmptyEntries);
+                execPathMultiple = execPathMultipleArr[0];
+                if (execPathMultipleArr.Length > 1)
+                {
+                    execPathMultipleArgs = execPathMultipleArr[1].Trim();
+                }
+            }
+            if (argsLength == 2)
+            {
+                execPathSingle = args[1];
+                var execPathSingleArr = execPathSingle.Split(new[] { "\"" }, StringSplitOptions.RemoveEmptyEntries);
+                execPathSingle = execPathSingleArr[0];
+                if (execPathSingleArr.Length > 1)
+                {
+                    execPathSingleArgs = execPathSingleArr[1].Trim();
+                }
+            }
+            else
+            {
+                Console.WriteLine("error: argument count cannot exceed 2, probably missing quotes in file path");
+                return 1;
+            }
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
             while (true)
             {
-                var screenCountCurrent = Screen.AllScreens.Length;
-                if (screenCountCurrent != screenLengthPrevious)
+                var screenCountCurrent = SystemInformation.MonitorCount;
+                if (screenCountCurrent != screenCountPrevious)
                 {
-                    Thread.Sleep(5000);
-                    if (screenCountCurrent <= 1)
+                    if (screenCountCurrent > 1)
                     {
-
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo()
+                            {
+                                FileName = execPathMultiple,
+                                Arguments = execPathMultipleArgs,
+                            });
+                        }
+                        catch
+                        {
+                        }
                     }
                     else
                     {
-
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo()
+                            {
+                                FileName = execPathSingle,
+                                Arguments = execPathSingleArgs,
+                            });
+                        }
+                        catch
+                        {
+                        }
                     }
                 }
-                screenLengthPrevious = screenCountCurrent;
-                Thread.Sleep(5000);
+                screenCountPrevious = screenCountCurrent;
+                Thread.Sleep(1000);
             }
         }
     }
